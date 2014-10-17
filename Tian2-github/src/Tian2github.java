fdc89d351c4c2cef4ac2a9999c54e14567473dbd
htian <htian@CIBMTR-182.mcwcorp.net>
2014-10-17 10:53:07 -0500

    Tian2github third push

--- Tian2-github/src/Tian2github.java
+++ Tian2-github/src/Tian2github.java
@@ -5,6 +5,7 @@ public class Tian2github {
 		// TODO Auto-generated method stub
 		System.out.println("This is my first try to link github. \n");
 		System.out.println("This is my second try to link github. \n");
+		System.out.println("This is my third try to link github. \n");
 	}
 
 }
