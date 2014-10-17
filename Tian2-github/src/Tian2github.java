9e26b3a32acb5db215c4698a3f80ba21a28e4f35
htian <htian@CIBMTR-182.mcwcorp.net>
2014-10-17 10:02:41 -0500

    push the project source to Tian2github version control

---
+++ Tian2-github/src/Tian2github.java
@@ -0,0 +1,9 @@
+
+public class Tian2github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				System.out.println("This is my first try to link github. \n");
+	}
+
+}
