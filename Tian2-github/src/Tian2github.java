b465def34a6e76d695adbaf3bed94b4e786466bf
htian <htian@CIBMTR-182.mcwcorp.net>
2014-10-17 10:46:19 -0500

    tian2github 2nd push

--- Tian2-github/src/Tian2github.java
+++ Tian2-github/src/Tian2github.java
@@ -3,7 +3,8 @@ public class Tian2github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				System.out.println("This is my first try to link github. \n");
+		System.out.println("This is my first try to link github. \n");
+		System.out.println("This is my second try to link github. \n");
 	}
 
 }
